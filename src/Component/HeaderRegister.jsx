3bcb48c66eea064c19e7b3106ad5ea4b6c805b3a
SuryaHuang <suryahuang1996@gmail.com>
2025-08-11 18:39:50 +0800

    Import Logo & css

--- src/Component/HeaderRegister.jsx
+++ src/Component/HeaderRegister.jsx
@@ -1,10 +1,11 @@
-// import "../Css/Login.css";
+import "../Css/Register.css";
+import Logo from "../images/Logo/Logo.png";
 
 const HeaderRegister = () => {
   return (
     <section>
       <div className="logo">
-        <img src="./src/images/Logo/Logo.png" alt="logo" />
+        <img src={Logo} alt="logo" />
       </div>
       <h2 className="header">Daftar</h2>
       <p className="description">Selamat datang!</p>
