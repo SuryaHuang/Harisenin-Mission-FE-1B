2fa8dd2fbbc928c44fce8c07d6b9dd5f1732c444
SuryaHuang <suryahuang1996@gmail.com>
2025-08-11 18:30:56 +0800

    Import Logo Image

--- src/Component/HeaderLogin.jsx
+++ src/Component/HeaderLogin.jsx
@@ -1,10 +1,11 @@
 import "../Css/Login.css";
+import Logo from "../images/Logo/Logo.png";
 
 const HeaderLogin = () => {
   return (
     <section>
       <div className="logo">
-        <img src="./src/images/Logo/Logo.png" alt="logo" />
+        <img src={Logo} alt="logo" />
       </div>
       <h2 className="header">Masuk</h2>
       <p className="description">Selamat datang kembali!</p>
